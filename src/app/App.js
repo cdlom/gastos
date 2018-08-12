b14cb71e391fe18a0148dd6c8d786b42ee2e99e7
cristianl <crisian.lombardi@gmail.com>
2018-08-12 13:17:22 -0300

    j

---
+++ src/app/App.js
@@ -0,0 +1,24 @@
+
+import React , { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="light-blue darken-4">
+                    <div className ="container">
+                        <a className="brand-logo" href="/"> Mern Stack!!</a>
+
+                    </div>
+
+                </nav>
+
+            </div>
+
+        )
+
+    }
+
+}
+
+export default App;
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,7 @@
-alert('react va aca');
+import React , { Component } from 'react';
+import {render} from 'react-dom';
+
+import App from './App'
+
+
+render(<App/>, document.getElementById('app'));
