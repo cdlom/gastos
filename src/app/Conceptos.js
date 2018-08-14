9f20a5f8674ef4fdcb7ff63f8cd69210a05c9ddf
cristianl <crisian.lombardi@gmail.com>
2018-08-14 20:34:02 -0300

    test

--- src/app/Conceptos.js
+++ src/app/Conceptos.js
@@ -3,8 +3,48 @@ import React , { Component} from 'react';
 class Conceptos extends Component{
     render(){
         return (
-            <div className = "Conceptos">
-                <h1>Conceptos</h1>
+            <div className="container">
+                <h3> Conceptos</h3>
+                <div className="row">
+                    <div className="col s12">
+                        <div className="card">
+                            <div className="card-content">
+                                <form onSubmit={this.addConcepto}>
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <input name="conceptodesc" onChange={this.handleChange} value={this.state.conceptodesc} placeholder="Ingrese un concepto" type="text" autoFocus />
+                                            <label for="conceptodesc">Descripción de concepto</label>
+                                        </div>
+                                    </div>
+
+                                    <div className="row">
+                                        <div>
+                                        <label for="group1">Tipo de Concepto</label>
+                                        
+                                        <div className="input-field col s12">
+                                            
+                                            <label>
+                                            <input name="group1" onChange={this.handleOptionChange} value="Ingreso"  type="radio" checked={this.state.conceptotipo==="Ingreso"}/>
+                                            <span>Ingreso</span>
+                                            </label>
+                                        </div>
+                                        <div className="input-field col s12">
+                                            <label>    
+                                            <input name="group1" onChange={this.handleOptionChange} value="Egreso"  type="radio" checked ={this.state.conceptotipo==="Egreso"} />
+                                            <span>Egreso</span>
+                                            </label>
+                                        </div>
+                                        </div>
+                                        
+                                    </div>
+
+                                </form>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
